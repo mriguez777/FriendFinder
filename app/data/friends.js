d73f68d0d598899e75dc8e58ef576f62b7149561
MROD <mriguez777@gmail.com>
2020-04-29 17:47:37 -0600

    brining over code from a previous working folder so that I can upload to github and then deploy on Heroku. Testing at this point to make sure the app runs before deployment

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+var friendsData = [
+  {
+    name: "Barf the Mawg",
+    photo:
+      "https://vignette.wikia.nocookie.net/p__/images/4/4e/Spaceballs4.jpg/revision/latest/scale-to-width-down/340?cb=20120225231815&path-prefix=protagonist",
+    scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3],
+  },
+  {
+    name: "Sweet Brother Numsie!",
+    photo:
+      "https://uploahttps://i.imgur.com/UneR0gp.gifd.wikimedia.org/wikipedia/en/thumb/e/e5/No_2_APTSWSM.jpg/200px-No_2_APTSWSM.jpg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  },
+  {
+    name: "The Joker",
+    photo:
+      "https://https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlxRH-aEeHeWlSGWNUmNAajBraiQDpZX0KCcJdSuCus4XJqceW&usqp=CAUs-media-cache-ak0.pinimg.com/564x/dd/31/d2/dd31d2dd156204fcf9b6aa1c54455adb.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  },
+  {
+    name: "Wonder Woman",
+    photo:
+      "http://vignetthttps://i.ytimg.com/vi/EvMp-YIkPX0/maxresdefault.jpge3.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest?cb=20060803214449",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  },
+  {
+    name: "Dormammu",
+    photo:
+      "https://at-cdn-s01.audiotool.com/2012/05/28/documents/ei368KjdlP52wCEAAkGBxMTEhUTExMWFhUXGRoZGBcXFxcdGhgYGBcXGBcYGBgYHiggGBolHRcaITEhJSkrLi4uFx8zODMtNygtLisBCgoKDg0OGxAQGy0mICYtLS8tLS0tLS0tLS8tLS0tLy0tLS8tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLfqy6gILDLtH9cEAf6H/0/cover256x256-b31436c2a9bc4645b1ad67bc09705cd7.jpg",
+    scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5],
+  },
+  {
+    name: "Nami",
+    photo:
+      "https://s-media-cache-ak0.pinimg.com/originals/a9/01/29/a9012982ehttps://static1.cbrimages.com/wordpress/wp-content/uploads/2019/12/Featured-Image-Nami-Cropped.jpgea651ee31e21f4ad11fe96b.jpg",
+    scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3],
+  },
+  {
+    name: "Leeloo",
+    photo:
+      "http://imagehttps://vignette.wikia.nocookie.net/fifth-element/images/6/69/Tumblr_nodbdlkH1I1rv91n5o1_500.png/revision/latest/scale-to-width-down/340?cb=20191007115149s.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+    scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4],
+  },
+];
+
+// Export to API Route
+module.exports = friendsData;
